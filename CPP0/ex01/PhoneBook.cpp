c9f96a2785ded2796ec9341d3d8d85c685f473f9
Ana Rita Dores <adores@c1r10s1.42lisboa.com>
2026-04-30 15:47:26 +0100

    changes

--- CPP0/ex01/PhoneBook.cpp
+++ CPP0/ex01/PhoneBook.cpp
@@ -3,45 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adores <adores@student.42.fr>              +#+  +:+       +#+        */
+/*   By: adores <adores@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 10:41:24 by adores            #+#    #+#             */
-/*   Updated: 2026/02/27 11:37:24 by adores           ###   ########.fr       */
+/*   Updated: 2026/04/30 12:19:22 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-class PhoneBook{
-	public:
-		Contact contact[8];
-		void add();
-		void search();
-		void exit();
-};
-
-class Contact {
-	public:
-		std::string first_name;
-		std::string last_name;
-		std::string nickname; 
-		std::string phone_number;
-		std::string darkest_secret;
-};
-
 void PhoneBook:: add()
 {
 	Contact new_contact;
 	std::cout << "First name: ";
-	std::cin >> new_contact.first_name; 
+	std::cin >> new_contact.first_name;
 	std::cout << "Last name: ";
 	std::cin >> new_contact.last_name;
 	std::cout << "Nickname: ";
-	std::cin >> new_contact.nickname;
+	std::getline(std::cin, new_contact.nickname);
 	std::cout << "Phone number: ";
 	std::cin >> new_contact.phone_number;
 	std::cout << "Darkest secret: ";
-	std::cin >> new_contact.darkest_secret;
+	std::getline(std::cin, new_contact.darkest_secret);
 	
 }
 
@@ -61,7 +44,7 @@ int main()
 	std:: string input;
 	while(1)
 	{
-		std:: cout << "Please enter ADD, SEARCH or EXIT: " << std:: endl;
+		std:: cout << "> Please enter ADD, SEARCH or EXIT: " << std:: endl;
 		std:: cin >> input;
 		if(input == "ADD")
 			contact1.add();
--- CPP0/ex01/PhoneBook.hpp
+++ CPP0/ex01/PhoneBook.hpp
@@ -1,2 +1,24 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
 #include <iostream>
 #include <string>
+
+class Contact {
+	public:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname; 
+		std::string phone_number;
+		std::string darkest_secret;
+};
+
+class PhoneBook{
+	public:
+		Contact contact[8];
+		void add();
+		void search();
+		void exit();
+};
+
+#endif
