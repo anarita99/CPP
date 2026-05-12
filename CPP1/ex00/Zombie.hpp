0de39da293e16f2d3959d5e5c7354222f2d434b9
anarita99 <anaritadores@gmail.com>
2026-05-12 17:20:09 +0100

    updated almost finish

---
+++ CPP1/ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adores <adores@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 14:53:50 by adores            #+#    #+#             */
+/*   Updated: 2026/05/12 14:56:08 by adores           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie{
+	private:
+		std::string name;
+	public:
+		void announce(void);
+}
+
+#endif
