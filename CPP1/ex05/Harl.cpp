6fbd0a6d1e25dc4298fc622fc0e830878b5dce29
Ana Rita Dores <adores@c1r10s3.42lisboa.com>
2026-05-20 16:43:28 +0100

    update

---
+++ CPP1/ex05/Harl.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adores <adores@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 14:59:32 by adores            #+#    #+#             */
+/*   Updated: 2026/05/20 16:20:31 by adores           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	std::cout << "Harl was born." << std::endl;
+}
+Harl::~Harl()
+{
+	std::cout << "Harl was destroyed." << std::endl;
+}
+
+void Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"<< std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"<< std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."<< std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now."<< std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	
+}
